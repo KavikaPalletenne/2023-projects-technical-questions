a941125ba97a9198e986f71febdcdc6f07f8a75a
Kavika Palletenne <kbpalletenne@gmail.com>
2023-01-08 20:07:59 +0530

    completed chaos q3

--- chaos/q1-backend/q3/src/main.rs
+++ chaos/q1-backend/q3/src/main.rs
@@ -10,9 +10,63 @@
 // - UNSW Session
 // - Birthdate
 // - Sex
-// 
+//
 // Write a Rust program to find the course which has the highest average student WAM.
 
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::fs::File;
+
+#[derive(Deserialize)]
+struct Record {
+    pub course_code: String,
+    pub student_number: i32,
+    pub name: String,
+    pub program: String,
+    pub plan: String,
+    pub wam: f32,
+    pub session: String,
+    pub birthdate: i32,
+    pub sex: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("C:\\Users\\kbpal\\Documents\\Development\\2023-projects-technical-questions\\chaos\\q1-backend\\student.psv").expect("Couldn't open file");
+    let mut reader = csv::ReaderBuilder::new()
+        .delimiter(b'|')
+        .has_headers(false)
+        .from_reader(file);
+
+    //                      Course Code, (WAM Total, count of WAMS added)
+    let mut course_wams: HashMap<String, (f32, i32)> = HashMap::new();
+
+    // Parse WAMs from file and save to HashMap
+    for record in reader.deserialize() {
+        let record: Record = record.expect("Could not parse input file");
+
+        if let Some((total, num)) = course_wams.get(&*record.course_code).cloned() {
+            course_wams.insert(record.course_code, (total + record.wam, num + 1));
+        } else {
+            course_wams.insert(record.course_code, (record.wam, 1));
+        }
+    }
+
+    let mut highest_average: f32 = 0.0;
+    let mut highest_wam_course_code: String = String::new();
+
+    // Calculate averages
+    for (course_code, (total, num)) in course_wams {
+        let average = total / (num as f32);
+
+        if average > highest_average {
+            highest_average = average;
+            highest_wam_course_code = course_code;
+        }
+    }
+
+    println!(
+        "Course with highest average WAM: {} | Average WAM: {}",
+        highest_wam_course_code,
+        highest_average,
+    );
 }
